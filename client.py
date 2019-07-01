adc26ebdffcef699a38a3f9f518f762b22fd8493
wattai <10399305+wattai@users.noreply.github.com>
2019-07-01 19:13:44 +0900

    fix to exit client.py properly.

--- client.py
+++ client.py
@@ -9,7 +9,7 @@ from socket import socket, AF_INET, SOCK_DGRAM, SOCK_STREAM
 
 HOST = "127.0.0.1"  # address to send.
 PORT = 5000
-
+exitwords = ['quit', 'exit']
 
 def run_by_udp():
     with socket(AF_INET, SOCK_DGRAM) as s:
@@ -27,11 +27,14 @@ def run_by_tcpip():
         s.connect((HOST, PORT))
         while True:
             msg = input("> ")
-            # send the message to the server.
-            s.sendall(msg.encode())
-            # buffer size is 1024.
-            data = s.recv(1024)
-            print(data.decode())
+            if msg in exitwords:
+                break
+            else:
+                # send the message to the server.
+                s.sendall(msg.encode())
+                # buffer size is 1024.
+                data = s.recv(1024)
+                print(data.decode())
 
 
 if __name__ == '__main__':
